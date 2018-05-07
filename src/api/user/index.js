75fa555d37d27e3826dba4408c6dbe6ce486d965
MarcioJS <marcio@startupboost.io>
2018-05-07 00:56:16 -0300

    Api fix

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -2,7 +2,7 @@ import request from 'superagent';
 
 function getUser() {
   return request
-    .get('http://mysterious-plains-40083.herokuapp.com/api/user')
+    .get('https://mysterious-plains-40083.herokuapp.com/api/user')
     .then(({ body: { age, name }}) => {
       return { age, name };
     });
